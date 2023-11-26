3a0fbd64035db839ddd85a2551029a7ad2fd3db4
Auriel James <aurieljames11@gmail.com>
2023-11-27 00:16:42 +0800

    issue/#1 Login User Interface is Done

---
+++ frontend/components/TheFooter.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+<div class="py-5">
+    <div class="flex w-full justify-center text-sm space-x-3 text-gray-700">
+        <a href="#" class="hover:underline">About</a>
+        <a href="#" class="hover:underline">Download the X app</a>
+        <a href="#" class="hover:underline">Help Center</a>
+        <a href="#" class="hover:underline">Terms of Service</a>
+        <a href="#" class="hover:underline">Privacy Policy</a>
+        <a href="#" class="hover:underline">Cookie Policy</a>
+        <a href="#" class="hover:underline">Accessibility</a>
+        <a href="#" class="hover:underline">Ads info</a>
+        <a href="#" class="hover:underline">Blog</a>
+        <a href="#" class="hover:underline">Status</a>
+        <a href="#" class="hover:underline">Careers</a>
+        <a href="#" class="hover:underline">Brand Resources</a> 
+        <a href="#" class="hover:underline">Advertising</a>
+        <a href="#" class="hover:underline">Martketing</a>
+        <a href="#" class="hover:underline">X for Business</a>
+        <a href="#" class="hover:underline">Developer</a>
+    </div>
+    <div class="flex justify-center space-x-4 text-sm text-gray-700">
+        <a href="#">Directory</a>
+        <a href="#">Settings</a>
+        <a href="https://facebook.com/aurieljames11">© 2023 Fernandez.</a>
+    </div>
+</div>
+</template>
+<style></style>
