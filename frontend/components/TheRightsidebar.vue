1b0edc29dfe50ac95f0119c43c0fab6adbb63a12
Auriel James <aurieljames11@gmail.com>
2023-12-03 23:53:06 +0800

    issue/#7B

---
+++ frontend/components/TheRightsidebar.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+<aside class="w-full">
+    <div class="flex justify-center items-center h-screen bg-neutral-800 w-full">
+        <h1 class="text-3xl text-white">Im left side bar</h1>
+    </div>
+</aside>
+</template>
+<style></style>
