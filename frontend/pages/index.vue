05e8a1394adea2b04e50e52f19cb68f4e5d6589d
Auriel James <aurieljames11@gmail.com>
2023-12-19 00:50:11 +0800

    issue/#31 Login UI Done

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,12 +5,14 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 
-const { logIn, errors, successMessage } = useUser();
+const { logIn, errors, successMessage, errorsMessage } = useUser();
 
 const initialState = {
     email: '',
     password: '',
 }
+
+const isError = ref(false);
 
 const toastSuccess = ref(''); 
 
@@ -43,6 +45,10 @@ const loginSubmitHandler = async() => {
             }
             navigateTo({ path: "/home" });
         }else{
+            isError.value = true;
+            setTimeout((()=>{
+                    isError.value = false;
+                }), 5000);
             if(errors.value.email[0]!==''){
                 loginErrors.email = true;
                 errorMessage.email = errors.value?.email[0];
@@ -58,6 +64,20 @@ const loginSubmitHandler = async() => {
 </script>
 <template>
 <div class="flex justify-around h-screen mt-5">
+    <div v-if="isError" class="absolute right-10 top-8 w-60 animate-bounce">
+        <div id="toast-warning" class="flex items-center max-w-xs px-7 py-2 text-gray-500 bg-red-100 border-t-4 border-red-500 rounded-md shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+        <div>
+            <span class="text-md font-bold text-red-600">Error</span>
+            <div class="text-xs text-red-700 font-normal duration-300">{{ errorsMessage }}</div>
+        </div>
+        <button type="button" class="ms-auto -mx-2.5 -my-1.5 text-red-800 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-red-300 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-warning" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+        </button>
+        </div>
+    </div>
     <div v-if="toastSuccess" class="absolute right-10 top-8 w-60 animate-bounce">
         <div id="toast-success" class="flex items-center max-w-xs px-7 py-2 text-gray-500 bg-green-100 border-t-4 border-green-500 rounded-md shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
         <div>
