26edd5a94b366425092feeaa20ef78509d1e74db
Auriel James <aurieljames11@gmail.com>
2024-07-25 21:51:01 +0800

    Trasfered the Logic from controller to LikeService

--- backend/app/Services/PostService.php
+++ backend/app/Services/PostService.php
@@ -20,5 +20,51 @@ class PostService
             ], 500);
         }
     }
+
+    public function post($request){
+        try{
+            if($request->image || $request->text){
+                $data['user_id'] = $request->user_id;
+                $data['text'] = $request->text;
+                $data['image'] = $request->image;
+
+                Post::create($data);
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Posted successfully',
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Unable action',
+                ], 400);
+            }
+        }catch(\Throwable $th){
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function update($request, $postId)
+    {
+        try{
+            $post = Post::find($postId);
+            $post->update([
+                'text' => $request->text,
+                'image' => $request->image,
+            ]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Post Updated Succesfully',
+            ], 200);
+        }catch(\Throwable $th){
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
     
 }
