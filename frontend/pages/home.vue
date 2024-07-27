1316fafdd51684e26b8bd4a64534d405f2cd4d43
Auriel James <aurieljames11@gmail.com>
2024-07-27 22:17:09 +0800

    Like Modal For HomePage Done

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -8,8 +8,6 @@ import { initFlowbite } from 'flowbite';
 const { logOut, getUser, userData } = useUser();
 const { post, errors, getAllPost, tweets, likepost, unlikepost } = usePost();
 
-const logInToastSuccess = ref("");
-
 defineProps({
     dark:{
         type: Boolean
@@ -35,6 +33,8 @@ const initialState = {
     'image': '',
 }
 
+
+const logInToastSuccess = ref("");
 const postForm = reactive({...initialState});
 
 const handlePost = async() => {
@@ -69,6 +69,7 @@ const tweetFileHandler = async(event) => {
 
 const activePost = ref("");
 const activeLike = ref("");
+const activeLiker = ref("");
 
 const setActivePost = (postId) => {
     activePost.value = postId;
@@ -79,12 +80,21 @@ const setActiveLike = (postId) => {
     activeLike.value = postId;
 } 
 
+const setActiveLiker = (likerId) => {
+    activeLiker.value = likerId;
+}
+
+
 const removeActivePost = () => {
     activePost.value = "";
 }
 
 const removeActiveLike = () => {
     activeLike.value = "";
+}
+
+const removeActiveLiker = () => {
+    activeLiker.value = "";
 }
 
 const handleLike = (postId) => {
@@ -98,6 +108,18 @@ const handleUnlike = (postId) => {
     unlikepost(userData.value.id, postId).then(()=>{
          getUser(userData.value);
     })
+}
+
+const likesForModal = ref([{}]);
+
+const handleLikeModal = (likes) => {
+    likesForModal.value = likes;
+
+    document.getElementById('like-modal').style.display = 'flex';
+}
+
+const hideLikeModal = () => {
+    document.getElementById('like-modal').style.display = 'none';
 }
 
 </script>
@@ -134,7 +156,7 @@ const handleUnlike = (postId) => {
 </div>
 
 <!-- Like modal -->
-<div id="like-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center w-full h-full bg-black bg-opacity-60">
+<div id="like-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-40 flex justify-center w-full h-full bg-black bg-opacity-60">
     <div class="relative p-4 w-full pt-20 max-w-2xl max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -143,7 +165,7 @@ const handleUnlike = (postId) => {
                 <h3 class="text-md font-bold text-red-600 dark:text-white">
                     Likes
                 </h3>
-                <button type="button" class="bg-gray-200 hover:bg-gray-300 text-gray-900 rounded-full text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+                <button @click="hideLikeModal()" type="button" class="bg-gray-200 hover:bg-gray-300 text-gray-900 rounded-full text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
@@ -151,8 +173,47 @@ const handleUnlike = (postId) => {
                 </button>
             </div>
             <!-- Modal body -->
-            <div class="p-4 md:p-5 space-y-4 min-h-[400px]">
-                
+            <div class="p-4 md:p-5 min-h-[400px] space-y-4">
+                <div v-for="(like, index) in likesForModal.slice().reverse()" :key="index"  class="flex justify-between">
+                    <div class="flex items-center space-x-2 text-md font-semibold">
+                        <img :src="like.user?.avatar" class="rounded-full w-[42px] h-[42px] md:w-[42px] md:h-[42px] object-cover"/>
+                        <NuxtLink :to="`/profile/${like.user?.email.split('@')[0]}`" class="hover:cursor-pointer hover:underline hover:duration-500 hover:transition-500" @mouseleave="removeActiveLiker" @mouseover="setActiveLiker(like.user?.id)">{{`${like.user?.firstname} ${like.user?.lastname}`}}</NuxtLink>
+                        <div :id="`popover-user-profile-like${like.user?.id}`" :class="`${like.user?.id === activeLiker ? 'visible transition-opacity opacity-100 duration-700 transition-700 animation-700' :  'invisible transition-opacity opacity-100 duration-700 transition-700 animation-700' }  absolute z-10 mt-[230px] left-[40px] md:left-[50px] inline-block w-[300px] text-sm text-gray-500 rounded-xl shadow-xl`">
+                        <div :class="`${dark? 'bg-gray-900':'bg-white'} p-3 rounded-lg border ${dark?'border-gray-700':''}`">
+                            <div class="flex items-center justify-between mb-2">
+                                <a href="#">
+                                    <img class="w-16 h-16 rounded-full object-cover" :src="like.user?.avatar" :alt="like.user?.firstname">
+                                </a>
+                                <div>
+                                    <button type="button" :class="`${dark? 'text-gray-900 bg-white hover:bg-gray-200 font-bold':'text-white bg-neutral-900 hover:bg-neutral-800 font-medium'} focus:ring-4 focus:ring-blue-300 rounded-full text-md px-4 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`">Follow</button>
+                                </div>
+                            </div>
+                            <p :class="`${dark?'text-gray-200':'text-gray-900'} text-base font-bold leading-none dark:text-white`">
+                                <a href="#">{{like.user?.firstname+' '+like.user?.lastname}}</a>
+                            </p>
+                            <p :class="`${dark?'text-gray-300':'text-gray-900'} mb-3 text-md font-normal`">
+                                <a href="#" class="hover:underline">@{{like.user?.firstname}}</a>
+                            </p>
+                            <p class="mb-4 text-sm">Default Bio <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">username.com</a>.</p>
+                            <ul class="flex text-sm space-x-3">
+                                <li class="me-2">
+                                    <a href="#" class="hover:underline space-x-1">
+                                        <span :class="`${dark ? 'text-gray-300':'text-gray-900'} font-semibold dark:text-white`">11</span>
+                                        <span :class="`${dark ? 'text-gray-300':'text-gray-900'}`">Following</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="hover:underline space-x-1">
+                                        <span :class="`${dark ? 'text-gray-300':'text-gray-900'} font-semibold dark:text-white`">379.4K</span>
+                                        <span :class="`${dark ? 'text-gray-300':'text-gray-900'}`">Followers</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>    
+                    </div>
+                    <button :class="`${dark? 'bg-white text-neutral-800 font-extrabold':'bg-neutral-900 text-white font-bold'} text-sm h-8 px-4 rounded-full`">Follow</button>
+                </div>
             </div>
         </div>
     </div>
@@ -270,9 +331,7 @@ const handleUnlike = (postId) => {
                         <svg v-if="tweet.like?.some((user)=> user.user_id === userData.id)" @click.prevent="handleUnlike(tweet.id)" width="26" height="26" style="color: red" xmlns="http://www.w3.org/2000/svg" class="w-5 group-hover:fill-red-200 duration-300 transition-300" viewBox="0 0 512 512"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" fill="red"></path></svg>
                         <svg v-else @click.prevent="handleLike(tweet.id)" stroke="black" stroke-width="0.2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" :fill="`${dark?'white':'black'}`" class="bi bi-heart group-hover:fill-red-700 duration-300 transition-300" viewBox="-2 -2 20 20"> <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/> </svg>
                     </div>
-                    <button v-if="tweet.like.length > 0"  type="button" data-modal-target="static-modal" data-modal-toggle="static-modal" :class="`text-sm ${dark?'text-white':'text-black'} font-semibold hover:text-red-800 duration-300 animation-300`" @mouseleave="removeActiveLike" @mouseover="setActiveLike(tweet.id)">{{tweet.like.length}}</button>
-                    
-
+                    <button v-if="tweet.like.length > 0" @click="handleLikeModal(tweet.like)" type="button" data-modal-target="static-modal" data-modal-toggle="static-modal" :class="`text-sm ${dark?'text-white':'text-black'} font-semibold hover:text-red-800 duration-300 animation-300`" @mouseleave="removeActiveLike" @mouseover="setActiveLike(tweet.id)">{{tweet.like.length}}</button>
                 </div>
                    <div :id="`likes-tooltip-${tweet.id}`" :class="`${activeLike==tweet.id ? 'visible' : 'invisible'} absolute z-10 px-3 py-2 ml-2 mt-6 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-100 dark:bg-gray-700`">
                         <b>Likes</b>
